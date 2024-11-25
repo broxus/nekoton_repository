4d5380e2e96fb0490bebcb48906078cc64125392
Andrey Molochko <36672245+AndreyMolochko@users.noreply.github.com>
2024-11-25 16:52:34 +0300

    fix: expiration time fix ci (#139)
    * update version
    
    * fix ci
    
    ---------
    
    Co-authored-by: Andrey Malochka <andrey.molocko2018@gmail.com>

--- lib/src/models/generic_contract/contract_updates_subscription.g.dart
+++ lib/src/models/generic_contract/contract_updates_subscription.g.dart
@@ -14,16 +14,8 @@ _$ContractUpdatesSubscriptionImpl _$$ContractUpdatesSubscriptionImplFromJson(
     );
 
 Map<String, dynamic> _$$ContractUpdatesSubscriptionImplToJson(
-    _$ContractUpdatesSubscriptionImpl instance) {
-  final val = <String, dynamic>{};
-
-  void writeNotNull(String key, dynamic value) {
-    if (value != null) {
-      val[key] = value;
-    }
-  }
-
-  writeNotNull('state', instance.contractState);
-  writeNotNull('transactions', instance.transactions);
-  return val;
-}
+        _$ContractUpdatesSubscriptionImpl instance) =>
+    <String, dynamic>{
+      if (instance.contractState case final value?) 'state': value,
+      if (instance.transactions case final value?) 'transactions': value,
+    };
