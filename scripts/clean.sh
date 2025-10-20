22a024b5f3fbe3588d40be537ce6be0687eb6d30
Egor Komarov <e.komarov.bf@gmail.com>
2025-10-20 13:06:31 +0200

    fix: update flutter_nekoton_bridge dependency to stable version and remove unused reflectable files from clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,5 +4,4 @@
 find . -type f -name "*_generated.*" -delete
 find . -type f -name "*.freezed.dart" -delete
 find . -type f -name "*.g.dart" -delete
-find . -type f -name "*.reflectable.dart" -delete
 find . -type f -name "*.module.dart" -delete
