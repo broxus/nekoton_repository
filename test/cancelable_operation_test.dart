6ec167b9b66d6621cffec077b7dee4e6dc9ea50f
Egor Komarov <e.komarov.bf@gmail.com>
2026-02-05 10:51:25 +0100

    test: remove unstable test

--- test/cancelable_operation_test.dart
+++ test/cancelable_operation_test.dart
@@ -1,7 +1,6 @@
 import 'package:async/async.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:nekoton_repository/src/utils/utils.dart';
 
 class FutureOperation {
   Future<void> call() async {}
@@ -11,25 +10,6 @@ class MockFunction extends Mock implements FutureOperation {}
 
 void main() {
   group('CancelableOperation', () {
-    test('Complete operation itself', () async {
-      final function = MockFunction();
-      when(
-        function.call,
-      ).thenAnswer((_) => Future.delayed(const Duration(milliseconds: 100)));
-
-      final start = NtpTime.now();
-      final operation = CancelableOperation<void>.fromFuture(function.call());
-
-      await operation.valueOrCancellation();
-      final end = NtpTime.now();
-
-      expect(
-        end.difference(start).inMilliseconds,
-        moreOrLessEquals(100, epsilon: 25),
-      );
-      verify(function.call).called(1);
-    });
-
     test('Complete operation by stop call', () async {
       final function = MockFunction();
       when(
